67fa346e7babd47a8fa6d4259d275f73e871588a
Fenriswolf200 <guri020406@gmail.com>
2025-08-25 11:32:48 +0200

    Added comments and masking but it is kind of slow

--- game/game_agent.py
+++ game/game_agent.py
@@ -1,14 +1,13 @@
 from collections import defaultdict
 import gymnasium as gym
 import numpy as np
- # hello world
-print("test")
+import pickle
 
 
 
 
 
-class BlackjackAgent:
+class MarbleGameAgent:
     def __init__(
         self,
         env: gym.Env,
@@ -58,16 +57,25 @@ class BlackjackAgent:
 
 
     # ---- Policy ----
-    def get_action(self, obs: dict):
+    def get_action(self, obs: dict, info: dict = None):
+
         s = self.encode_obs(obs)
 
-        if np.random.random() < self.epsilon:
-            # Random legal action in env's expected format (length-3 array/tuple)
-            return self.env.action_space.sample()
+        if info is not None and "action_mask" in info:
+            mask = info["action_mask"]
+            valid_indices = np.where(mask == 1)[0]
         else:
-            # Greedy flat index -> convert back to (r,c,d)
-            best_flat = int(np.argmax(self.q_values[s]))
-            return np.array(self.unflatten_action(best_flat), dtype=int)
+            valid_indices = np.arange(len(self.q_values[s]))
+
+
+        if np.random.random() < self.epsilon:
+            flat_action = np.random.choice(valid_indices)
+        else:
+            q_vals = self.q_values[s]
+            flat_action = valid_indices[np.argmax(q_vals[valid_indices])]
+
+        return np.array(self.unflatten_action(flat_action), dtype=int)
+
 
     # ---- Learning ----
     def update(
@@ -90,7 +98,87 @@ class BlackjackAgent:
         self.q_values[s][a_flat] += self.lr * td
         self.training_error.append(td)
 
+
+    def save(self, path: str):
+        """Save agent parameters and Q-table to disk."""
+        data = {
+            "q_values": dict(self.q_values),   # convert defaultdict -> normal dict
+            "lr": self.lr,
+            "discount_factor": self.discount_factor,
+            "epsilon": self.epsilon,
+            "epsilon_decay": self.epsilon_decay,
+            "final_epsilon": self.final_epsilon,
+            "nvec": self.nvec,
+            "num_actions": self.num_actions,
+        }
+        with open(path, "wb") as f:
+            pickle.dump(data, f)
+
+    @classmethod
+    def load(cls, path: str, env: gym.Env):
+        """Load agent from disk and attach to env."""
+        with open(path, "rb") as f:
+            data = pickle.load(f)
+
+        # Create a fresh agent with same params
+        agent = cls(
+            env=env,
+            learning_rate=data["lr"],
+            initial_epsilon=data["epsilon"],  # resume from last epsilon
+            epsilon_decay=data["epsilon_decay"],
+            final_epsilon=data["final_epsilon"],
+            discount_factor=data["discount_factor"],
+        )
+
+        # Restore learned Q-values
+        agent.q_values = defaultdict(
+            lambda: np.zeros(agent.num_actions, dtype=np.float32),
+            data["q_values"],
+        )
+
+        return agent
+
+
     def decay_epsilon(self):
         self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
 
+    def test(self, env, n_episodes=100, render=False):
+        rewards = []
+        successes = 0
+
+        for ep in range(n_episodes):
+            obs, info = env.reset()
+            done = False
+            total_reward = 0
+
+            while not done:
+                # Always greedy (epsilon = 0 in test mode)
+                action = self.get_action(obs, info)
+
+                next_obs, reward, terminated, truncated, info = env.step(action)
+                total_reward += reward
+                obs = next_obs
+
+                if render:
+                    env.render()
+
+                # 🚨 Ensure we break immediately when episode ends
+                done = terminated or truncated
+                if done:
+                    break
+
+            rewards.append(total_reward)
+
+            # success condition (custom to your env)
+            if "remaining marbles" in info and info["remaining marbles"] == 1:
+                successes += 1
+
+        avg_reward = np.mean(rewards)
+        success_rate = successes / n_episodes
+
+        print(f"✅ Test Results: {n_episodes} episodes")
+        print(f"   Average Reward: {avg_reward:.2f}")
+        print(f"   Success Rate:   {success_rate*100:.1f}%")
+
+        return avg_reward, success_rate
 
--- game/game_env.py
+++ game/game_env.py
@@ -3,16 +3,37 @@ import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
 from gymnasium.envs.registration import register
-from game_agent import BlackjackAgent
+from game_agent import MarbleGameAgent
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 
-
-# add action masking 
-# - make a function to obtain allowed moves and return a list of those allowed moves
-# - modify action space to only come from that list of allowed moves 
+# 1. Clean up code
+# 2. Add action masking 
+# - write function to get all valid moves and return a list with their np coordinates
+# - encode mask for your action space
+# - provide mask in reset and step()
+# - modify get_action to use mask
 # - Make function to terminate when no moves remain
-#
+# 3. Find a better reward system for the agent so that it learns faster
+# 4. Add logs to improve debugging
+
+
+ 
+"""
+
+
+
+board =
+[
+[2,2,1,1,1,2,2]
+[2,2,1,1,1,2,2]
+[1,1,1,1,1,1,1]
+[1,1,1,0,1,1,1]
+[1,1,1,1,1,1,1]
+[2,2,1,1,1,2,2]
+[2,2,1,1,1,2,2]
+]
+"""
 
 
 register(
@@ -27,33 +48,38 @@ class MarbleGameEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 30}
 
 
+    # --------------Initialization-------------------
+    # The init function creates the variables that will be used across the 
+    # program and sets them to their default state.
     def __init__(self, size=7, render_mode=None, fps=None):
 
-        self.size = size
-        self.board = None
-
-
-        self.cell_size = 64
+        # These two variables control the creation and management of 
+        # the board.        
+        self.size = size # Size of the board array. 
+        self.board = None # Will store a numpy 2D array that contains the "Board".
+        
+        # These variables control the rendering of the game in pygame.
+        self.cell_size = 64 
         self.margin = 24
         self.window = None
         self.clock = None
         self._canvas = None
-        self.wrong_move_counter = 10
-        
-        
-
         self.render_mode = render_mode
-
         if fps is not None:
             self.metadata["render_fps"] = fps
-
+        
+        # These variables will control the amount of wrong moves that the model will 
+        # be allowed to make consecutively before it is terminated.
+        self.wrong_move_counter = 0
+        self.max_wrong_moves = 10
 
         # Observations are dictionaries with the agent's and the target's location.
         # Each location is encoded as an element of {0, ..., `size`}^2,
-        # i.e. MultiDiscrete([size, size]).
         self.observation_space = spaces.Box(low=0, high=2, shape=(self.size, self.size), dtype=np.int8)
 
         
+
+        # An action space is an object that tells the agent what actions it has available. 
         # row (0-6)
         # column (0-6)
         # direction (0-3) up, down, left, right
@@ -61,7 +87,7 @@ class MarbleGameEnv(gym.Env):
         
 
 
-    
+        # This dictionary stores the movement vectors for each of the directions that a marble can move.
         self.movement_vectors = {
             0: np.array([0,-2]),
             1: np.array([0,2]),
@@ -69,15 +95,55 @@ class MarbleGameEnv(gym.Env):
             3: np.array([2,0])
         }
         
+
+        # This function will initialize all the variables to their default values
         self.reset()
 
+    # The reset function will set all of the variables in the environment 
+    # to their default state and it will return the info on the environment.
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+
+        self.board = self.create_board(self.size)   
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+
+        return observation, info
+    
+
+    # --------------Analytics/Logs-------------------
+    # _get_obs and _get_info are functions that will return info about the environment
+    # _get_obs is used by the agent to view the environment and make it's decisions from there.
+    # _get_info is used for debugging
     def _get_obs(self):
         return {"board":self.board}
     
     def _get_info(self):
+        valid_actions = self.get_valid_moves()
+        mask = np.zeros(self.size * self.size * 4, dtype=np.int8)
+        for a in valid_actions:
+            mask[self.action_to_index(a)] = 1
+
+        
         return {
-            "remaining marbles":(np.sum(self.board == 1))
+            "remaining marbles":(np.sum(self.board == 1)),
+            "action_mask":mask
         }
+
+
+    # --------------Board----------------------------
+    # This function will create a 2D array that will act as the board for the game.
+    # - The size of the board will be defined by the parameter size with the default being 7x7
+    # - Each tile will have a value in the range of [0,2]
+    #   - 0 means that the tile is empty.
+    #   - 1 means that the tile has a marble in it.
+    #   - 2 means that the tile is blocked and not playable
+    # - When creating the board the function will set every tile in the array to 1. 
+    #   Then it will set the center tile to 0. Finally it will block all of the 
+    #   tiles on the edges by setting them to 2
+    # - Once all of this is done the function will return the board to be used in the game
 
     def create_board(self, size=7):
         board = np.full((size,size),1, dtype=np.int8)
@@ -98,87 +164,114 @@ class MarbleGameEnv(gym.Env):
         return board
 
 
-        
+
+    # --------------Movement-------------------------
+    # The check_movement_validity, as the name implies, will run tests to see if a movement is valid. 
     def check_movement_validity(self, selected_marble:np.array, new_position:np.array, midpoint:np.array):
 
         # 1. Check that selected token is a token and it is not empty or a wall
         if (self.board[selected_marble[1]][selected_marble[0]] != 1):
             return False
         
-        # 2. Check that the direction that the new position is in bounds
+        # 2. Check that the new position is inside the bounds of the board.
         if (new_position[0]<0) or (new_position[0] >= self.size) or (new_position[1]<0) or (new_position[1] >= self.size):
             return False
 
         # 3. Check that there is a marble in the midpoint
-        if self.board[selected_marble[1]][selected_marble[0]] != 1:
+        if self.board[midpoint[1]][midpoint[0]] != 1:
             return False
 
         # 4. Check that the new position is not a wall or blocked by another piece
-        if (self.board[new_position[1]][new_position[0]] != 1):
+        if (self.board[new_position[1]][new_position[0]] != 0):
             return False
 
         return True
-            
 
 
+    def get_valid_moves(self):
+        valid_moves = []
+        
+        for y in range(self.size):
+            for x in range(self.size):
+                for direction_index in self.movement_vectors:
+                    selected_marble = np.array([y,x])
+                    new_position = np.add(selected_marble, self.movement_vectors[direction_index])
+                    midpoint = np.add(selected_marble, (self.movement_vectors[direction_index]//2).astype(int))
+                    if self.check_movement_validity(selected_marble, new_position, midpoint):
+                        valid_moves.append((y,x,direction_index))
+        return valid_moves
+    
+    def action_to_index(self, action):
+        y, x, direction_index = action
+        return (y * self.size * 4) + (x * 4) + direction_index
+    
+    def index_to_action(self, index:int):
+        y = index // (self.size * 4)
+        x = (index % (self.size * 4)) // 4
+        direction_index = index % 4
+
+        return (y,x,direction_index)
+
+    
+
+
+    # MARBLE GAME STEP FUNCTION
+    # 1. User inputs coordinates for the selected piece and the action Enum that it is going to take (x,y,direction)
+    # 2. Direction object is converted to an np vector
+    # 3. Check to see if movement is valid
+    #     if it is +1 points if it isn't -0.1 points
+    # 4. Update Board and current position
+    # 5. Check to see if only one piece remains in the board np.sum and terminate if so,
+    # 6. Screen is rendered 
     def step(self, step_input):
-
-
-        """
-        MARBLE GAME STEP FUNCTION
-        1. User inputs coordinates for the selected piece and the action Enum that it is going to take (x,y,direction)
-        2. Direction object is converted to an np vector
-        3. Check to see if movement is valid
-            if it is +1 points if it isn't -0.1 points
-        4. Update Board and current position
-        5. Check to see if only one piece remains in the board np.sum and terminate if so,
-        6. Screen is rendered 
-        """
-
-
-
-
-
+        
         direction = int(step_input[2])
         selected_marble = np.array([step_input[0], step_input[1]])
 
-
-
-        
-
-
         new_position = np.add(selected_marble, self.movement_vectors[direction])
         midpoint = np.add(selected_marble, (self.movement_vectors[direction] // 2).astype(int))
+
+        reward = 0
 
 
         if self.check_movement_validity(selected_marble=selected_marble, new_position=new_position, midpoint=midpoint):
             self.board[midpoint[1]][midpoint[0]] = 0
             self.board[new_position[1]][new_position[0]] = 1
             self.board[selected_marble[1]][selected_marble[0]] = 0
-            reward = 1.0
+            reward += self.max_wrong_moves
             self.wrong_move_counter = 0
-
         else:
-
-             
             self.wrong_move_counter += 1
-            reward = -0.5
-            #print("WRONG MOVE")
-
+            reward -= 0.75
         
+
         observation = self._get_obs()
         info = self._get_info()
-        terminated = True if (info["remaining marbles"] == 1 or self.wrong_move_counter > 5) else False
-        
+
+        if info["remaining marbles"] == 1:
+            terminated = True
+            reward += 100.0
+        elif self.wrong_move_counter > self.max_wrong_moves:
+            terminated = True
+            reward -= 1.0
+
+        else:
+            terminated = False
 
 
-        self.render()
+
+        if self.render_mode == "human":
+            self.render()
 
 
         return observation, reward, terminated, False, info
 
+
+
+    # --------------Rendering-----------------------
+
     def render(self):
-        import pygame
+
         if self.board is None:
             return None
 
@@ -287,33 +380,29 @@ class MarbleGameEnv(gym.Env):
             self.clock = None
         # Don't call pygame.quit() globally if other envs/windows might exist
 
-    def reset(self, seed=None, options=None):
-        super().reset(seed=seed)
-
-        self.board = self.create_board(self.size)   
-
-        observation = self._get_obs()
-        info = self._get_info()
-        reward = 0
-        terminated = False
 
 
-        return observation, info
 
-        
+
+
 if __name__=="__main__":
     learning_rate = 0.01        # How fast to learn (higher = faster but less stable)
-    n_episodes = 100_00        # Number of hands to practice
+    n_episodes = 1_000_000        # Number of hands to practice
     start_epsilon = 1.0         # Start with 100% random actions
     epsilon_decay = start_epsilon / (n_episodes / 2)  # Reduce exploration over time
     final_epsilon = 0.1         # Always keep some exploration
 
 
+
+    """
+
+    # CODE TO TRAIN WITHOUT RENDERING
+
     # Create environment and agent
-    env = gym.make("MarbleGameEnv", render_mode="human")
+    env = gym.make("MarbleGameEnv")
     env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
 
-    agent = BlackjackAgent(
+    agent = MarbleGameAgent(
         env=env,
         learning_rate=learning_rate,
         initial_epsilon=start_epsilon,
@@ -344,11 +433,67 @@ if __name__=="__main__":
         #print(counter)
 
 
+    agent.save("MarbleGameAgent.pkl")
 
     env.close()
 
+
+
+
+
+    env = gym.make("MarbleGameEnv", render_mode="human")
+    env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
+    agent = MarbleGameAgent.load("MarbleGameAgent.pkl", env)
+
+
+    
+    counter = 0
+    for episode in tqdm(range(100)):
+
+        obs, info = env.reset()
+        done = False
+
+        while not done:
+            action = agent.get_action(obs)
+
+            next_obs, reward, terminated, _, info = env.step(action)
+
+            agent.update(obs=obs, action=action, reward=reward, terminated=terminated, next_obs=next_obs)
+
+
+            done = terminated
+            obs = next_obs
+
+        agent.decay_epsilon()
+        counter += 1
+        #print(counter)
+
+
+
+
+    env.close()
+
+    
+
+    """
+
+    env = gym.make("MarbleGameEnv", render_mode="human")  # headless test
+    agent = MarbleGameAgent.load("MarbleGameAgent.pkl", env)
+
+    agent.test(env, n_episodes=1_000, render=False)
+
+
+    
+
+
+
+    """
+
+
+
+
     def get_moving_avgs(arr, window, convolution_mode):
-        """Compute moving average to smooth noisy data."""
+        #Compute moving average to smooth noisy data.
         return np.convolve(
             np.array(arr).flatten(),
             np.ones(window),
@@ -397,4 +542,4 @@ if __name__=="__main__":
 
 
 
-
+    """
